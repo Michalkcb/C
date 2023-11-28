a10614e064ed16d0bc24be9357aeab4993dc5e44
Michalkcb <michalkcb@gmail.com>
2023-11-28 23:04:41 +0100

    add c01 ex01

---
+++ 42Warszawa/C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/27 10:47:35 by mbany             #+#    #+#             */
+/*   Updated: 2023/11/28 14:24:02 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+// int main()
+// {
+//     int a;
+//     int *pointer1 = &a;
+//     int **pointer2 = &pointer1;
+//     int ***pointer3 = &pointer2;
+//     int ****pointer4 = &pointer3;
+//     int *****pointer5 = &pointer4;
+//     int ******pointer6 = &pointer5;
+//     int *******pointer7 = &pointer6;
+//     int ********pointer8 = &pointer7;
+//     int *********pointer9 = &pointer8;
+
+//     ft_ultimate_ft(pointer9);
+
+//     printf("Wartosc a po wywolaniu funkcji: %d\n", a);
+
+//     return 0;
+// }
