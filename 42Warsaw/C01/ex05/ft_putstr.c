b340270b7d99aa3b93c121d6b819212d06040578
Michalkcb <michalkcb@gmail.com>
2023-11-29 20:40:10 +0100

    add c01 ex05

---
+++ 42Warsaw/C01/ex05/ft_putstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/27 10:47:35 by mbany             #+#    #+#             */
+/*   Updated: 2023/11/28 14:29:12 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putstr(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	write(1, str, i);
+}
+
+// int main()
+// {
+//     char myString[] = "Error: command prohibited! \n";
+
+//     ft_putstr(myString);
+
+//     return 0;
+// }
