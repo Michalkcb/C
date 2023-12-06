e15c62fbb804eb2e9c4d000afeeb17c7b013b3d4
Michalkcb <michalkcb@gmail.com>
2023-12-06 11:22:53 +0100

    add c00 ex02

---
+++ 42Warsaw/C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 14:50:27 by mbany             #+#    #+#             */
+/*   Updated: 2023/12/05 15:51:24 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	int	i;
+
+	i = 'z';
+	while (i >= 'a')
+	{
+		write(1, &i, 1);
+		i--;
+	}
+}
+
+// int main()
+// {
+// 	ft_print_reverse_alphabet();
+// 	return(0);
+// }
