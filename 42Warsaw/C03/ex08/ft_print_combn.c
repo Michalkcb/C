5be96fa239689a0e1bc87940b38a38d1eb1a2018
Michalkcb <michalkcb@gmail.com>
2023-12-08 23:23:47 +0100

    add c03 ex08

---
+++ 42Warsaw/C03/ex08/ft_print_combn.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 14:56:43 by mbany             #+#    #+#             */
+/*   Updated: 2023/12/05 18:11:38 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdbool.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print(int *tab, int n)
+{
+	bool	low;
+	int		i;
+
+	i = 1;
+	low = 1;
+	while (i < n)
+	{
+		if (tab[i - 1] >= tab[i])
+			low = 0;
+		i++;
+	}
+	if (low)
+	{
+		i = 0;
+		while (i < n)
+			ft_putchar(tab[i++] + 48);
+		if (tab[0] < (10 - n))
+		{
+			ft_putchar(',');
+			ft_putchar(' ');
+		}
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	int	i;
+	int	tab[9];
+
+	i = 0;
+	while (i < n)
+		tab[i++] = 0;
+	while (tab[0] <= (10 - n) && n >= 1 && n < 10)
+	{
+		print(tab, n);
+		tab[n - 1]++;
+		i = n - 1;
+		while (i && n > 1)
+		{
+			if (tab[i] > 9)
+			{
+				tab[i - 1]++;
+				tab[i] = 0;
+			}
+			i--;
+		}
+	}
+}
+
+// int	main(void)
+// {
+// 	ft_print_combn(5);
+// }
