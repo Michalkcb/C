6291cbab5be46264e5c3294e94f3283647945e98
Michalkcb <michalkcb@gmail.com>
2023-12-14 17:38:11 +0100

    add c06 ex00

---
+++ 42Warsaw/C06/ex00/ft_print_program_name.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 09:49:14 by mbany             #+#    #+#             */
+/*   Updated: 2023/12/07 10:39:06 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc > 0)
+	{
+		while (argv[0][i] != 0)
+		{
+			ft_putchar(argv[0][i]);
+			i++;
+		}
+		ft_putchar('\n');
+	}
+	return (0);
+}
