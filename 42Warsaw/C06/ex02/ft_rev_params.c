8a3128904c66bb7d3ca2c633f8c24520f29e80fb
Michalkcb <michalkcb@gmail.com>
2023-12-14 17:39:15 +0100

    add c06 ex02

---
+++ 42Warsaw/C06/ex02/ft_rev_params.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 09:52:30 by mbany             #+#    #+#             */
+/*   Updated: 2023/12/07 14:04:45 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = argc - 1;
+	while (i > 0)
+	{
+		j = 0;
+		while (argv[i][j] != 0)
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		i--;
+		ft_putchar('\n');
+	}
+	return (0);
+}
