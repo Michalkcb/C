065c1ba85807264e171801ad586fe835661fde5c
Michalkcb <michalkcb@gmail.com>
2023-12-14 17:38:45 +0100

    add c06 ex01

---
+++ 42Warsaw/C06/ex01/ft_print_params.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 09:50:06 by mbany             #+#    #+#             */
+/*   Updated: 2023/12/07 11:50:36 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j] != 0)
+			{
+				ft_putchar(argv[i][j]);
+				j++;
+			}
+			i++;
+			ft_putchar('\n');
+		}
+	}
+	return (0);
+}
