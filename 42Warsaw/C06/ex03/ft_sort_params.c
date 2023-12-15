d2786b03f0f2836f93a44c47b2947f81086ec197
Michalkcb <michalkcb@gmail.com>
2023-12-15 23:59:27 +0100

    add c06 ex03

---
+++ 42Warsaw/C06/ex03/ft_sort_params.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 09:52:57 by mbany             #+#    #+#             */
+/*   Updated: 2023/12/11 11:09:41 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_print_args(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		i++;
+		ft_putchar('\n');
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*arg;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i;
+		while (ft_strcmp(argv[j], argv[j - 1]) < 0 && j > 1)
+		{
+			arg = argv[j - 1];
+			argv[j - 1] = argv[j];
+			argv[j] = arg;
+			j--;
+		}
+		i++;
+	}
+	ft_print_args(argc, argv);
+	return (0);
+}
